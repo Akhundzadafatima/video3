88624cb7e5c74829966b290b4ac0e4da5be4d4fd
Akhundzadafatima <165835698+Akhundzadafatima@users.noreply.github.com>
2024-10-24 15:05:20 +0200

    Add files via upload

---
+++ 3-HttpRequest/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <main>
+  <div class="container">
+    <h3>Alış-veriş listi 
+    </h3>
+    <hr>
+      <input type="text" placeholder="Nə alacaqsınız?" v-model="inp" @keydown.enter="onSave">
+ 
+    <ul>
+      <li v-for="item in li" key="item">
+        <span>{{item}}</span>
+      <button class="delete">Sil</button>
+      </li>
+      <li>
+        <span>App2</span>
+      <button class="delete">Sil</button>
+      </li>
+      <li>
+        <span>App3</span>
+      <button class="delete">Sil</button>
+      </li>
+      <small>5 ədəd məhsul var</small>
+    </ul>
+  </div>
+</main>
+</template>
+
+<script>
+import axios from "axios"
+export default{
+  data(){
+    return{
+  
+    }
+  },
+methods:{
+  onSave(e){
+  }
+}
+
+}
+</script>
